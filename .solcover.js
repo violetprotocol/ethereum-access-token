1df0432e96cdecd19d252aed652282f4aaec4a46
ra-phael <10075759+ra-phael@users.noreply.github.com>
2022-07-26 12:00:46 +0100

    fix: compilation for tests

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,7 @@ const shell = require("shelljs");
 
 module.exports = {
   istanbulReporter: ["html", "lcov"],
+  configureYulOptimizer: true,
   providerOptions: {
     mnemonic: process.env.MNEMONIC,
   },
