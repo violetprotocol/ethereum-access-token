654fff7c25b52beec7ebddd15d276818166aa84f
ra-phael <10075759+ra-phael@users.noreply.github.com>
2023-05-02 11:46:30 +0100

    feat: add ability to update the verifier address

--- contracts/examples/DummyDapp.sol
+++ contracts/examples/DummyDapp.sol
@@ -4,8 +4,17 @@ pragma solidity ~0.7.6;
 import "../AccessTokenConsumer.sol";
 
 contract DummyDapp is AccessTokenConsumer {
+    address private _owner;
+
     // solhint-disable-next-line no-empty-blocks
-    constructor(address verifier) AccessTokenConsumer(verifier) {}
+    constructor(address verifier) AccessTokenConsumer(verifier) {
+        _owner = msg.sender;
+    }
+
+    function updateVerifier(address newVerifier) external {
+        require(msg.sender == _owner, "Unauthorized");
+        super.setVerifier(newVerifier);
+    }
 
     function lend(
         uint8 v,
