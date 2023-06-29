008dd5ddeca0cd6b2534509f15f34f38717247ac
Papa Smurf <chris@violet.co>
2023-06-29 17:43:46 +0100

    feat: added tasks to use OZ relayer to activate and deactivate issuers

---
+++ tasks/relayer/activateIssuer.ts
@@ -0,0 +1,24 @@
+import { task } from "hardhat/config";
+import { TaskArguments } from "hardhat/types";
+import { DefenderRelaySigner, DefenderRelayProvider } from "@openzeppelin/defender-relay-client/lib/ethers";
+import { AccessTokenVerifier } from "../../src/types";
+
+task("relay:activateIssuers", "Uses OZ Relayer to activate issuers")
+  .addParam("verifiercontract", "Address of the verifier contract")
+  .addParam("newissuer", "Address of the issuer to add")
+  .setAction(async (taskArguments: TaskArguments, hre) => {
+    if (!process.env.OZ_RELAY_API_KEY || !process.env.OZ_RELAY_API_SECRET)
+      throw new Error("Please set OZ_RELAY_API_KEY and OZ_RELAY_API_KEY env vars");
+
+    const credentials = { apiKey: process.env.OZ_RELAY_API_KEY, apiSecret: process.env.OZ_RELAY_API_SECRET };
+    const provider = new DefenderRelayProvider(credentials);
+    const signer = new DefenderRelaySigner(credentials, provider, { speed: "fast" });
+
+    const accessTokenVerifier = <AccessTokenVerifier>(
+      await hre.ethers.getContractAt("AccessTokenVerifier", taskArguments.verifiercontract, signer)
+    );
+    const tx = await accessTokenVerifier.activateIssuers([taskArguments.newissuer]);
+    const mined = await tx.wait();
+    console.log("Transaction has been mined.");
+    console.log("Transaction hash", mined.transactionHash);
+  });
