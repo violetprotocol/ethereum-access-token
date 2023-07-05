9445a04d18b7fed0fd8e1a5e6f30e7b7c6b4bf9d
Papa Smurf <chris@violet.co>
2023-07-05 11:53:59 +0100

    feat: adds hd version of intermediate key rotation

--- tasks/keyInfraSetters.ts
+++ tasks/keyInfraSetters.ts
@@ -2,6 +2,7 @@ import { ContractTransaction } from "ethers";
 import { task } from "hardhat/config";
 import { TaskArguments } from "hardhat/types";
 import { AccessTokenVerifier } from "../src/types";
+import { LedgerSigner } from "@anders-t/ethers-ledger";
 
 task("rotate:IntermediateKey")
   .addParam("verifiercontract", "Address of the Access Token Verifier Contract")
@@ -19,6 +20,40 @@ task("rotate:IntermediateKey")
     let tx: ContractTransaction | null = null;
     try {
       tx = await accessTokenVerifier.rotateIntermediate(taskArguments.newintermediate);
+      const receipt = await tx.wait();
+
+      if (receipt?.status === 1) {
+        console.log("Updated intermediate key to: ", newIntermediateKey);
+        console.log("TX hash", tx.hash);
+      } else {
+        throw new Error(`Transaction reverted. Tx hash: ${tx?.hash}`);
+      }
+    } catch (error) {
+      if (tx?.hash) {
+        console.error(`Error while rotating the intermediate key. Transaction hash: ${tx.hash}`);
+      }
+      throw error;
+    }
+  });
+
+task("hd:rotate:IntermediateKey")
+  .addParam("verifiercontract", "Address of the Access Token Verifier Contract")
+  .addParam("newintermediate", "Address of the new intermediate key")
+  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
+    const newIntermediateKey = taskArguments.newintermediate;
+    if (!newIntermediateKey || newIntermediateKey === ethers.constants.AddressZero) {
+      throw new Error("Invalid new intermediate key");
+    }
+
+    const ledger = new LedgerSigner(ethers.provider);
+
+    const accessTokenVerifier = <AccessTokenVerifier>(
+      await ethers.getContractAt("AccessTokenVerifier", taskArguments.verifiercontract)
+    );
+
+    let tx: ContractTransaction | null = null;
+    try {
+      tx = await accessTokenVerifier.connect(ledger).rotateIntermediate(taskArguments.newintermediate);
       const receipt = await tx.wait();
 
       if (receipt?.status === 1) {
