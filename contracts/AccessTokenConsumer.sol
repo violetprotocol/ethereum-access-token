c1c4f2f4407231a8b2ffcb3f6c71a65d1df8537c
Papa Smurf <chris@violet.co>
2023-07-10 09:59:46 +0100

    chore: used explicit imports to silence linter

--- contracts/AccessTokenConsumer.sol
+++ contracts/AccessTokenConsumer.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.13;
 
-import "./IAccessTokenVerifier.sol";
+import { IAccessTokenVerifier, AccessToken, FunctionCall } from "./IAccessTokenVerifier.sol";
 
 contract AccessTokenConsumer {
     IAccessTokenVerifier private immutable _verifier;
@@ -23,12 +23,7 @@ contract AccessTokenConsumer {
         _;
     }
 
-    function verify(
-        uint8 v,
-        bytes32 r,
-        bytes32 s,
-        uint256 expiry
-    ) internal view returns (bool) {
+    function verify(uint8 v, bytes32 r, bytes32 s, uint256 expiry) internal view returns (bool) {
         // AU -> Already Used
         require(!_isAccessTokenUsed(v, r, s, expiry), "AccessToken: AU");
 
@@ -72,22 +67,12 @@ contract AccessTokenConsumer {
         }
     }
 
-    function _isAccessTokenUsed(
-        uint8 v,
-        bytes32 r,
-        bytes32 s,
-        uint256 expiry
-    ) internal view returns (bool) {
+    function _isAccessTokenUsed(uint8 v, bytes32 r, bytes32 s, uint256 expiry) internal view returns (bool) {
         bytes32 accessTokenHash = keccak256(abi.encodePacked(v, r, s, expiry));
         return _accessTokenUsed[accessTokenHash];
     }
 
-    function _consumeAccessToken(
-        uint8 v,
-        bytes32 r,
-        bytes32 s,
-        uint256 expiry
-    ) private {
+    function _consumeAccessToken(uint8 v, bytes32 r, bytes32 s, uint256 expiry) private {
         bytes32 accessTokenHash = keccak256(abi.encodePacked(v, r, s, expiry));
 
         _accessTokenUsed[accessTokenHash] = true;
