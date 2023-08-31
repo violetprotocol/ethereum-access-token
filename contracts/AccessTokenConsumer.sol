cd9ad3745721e8cbf241c8ed391c9589fef911ab
Raphael <10075759+ra-phael@users.noreply.github.com>
2023-08-31 09:55:56 +0100

    Add comments (#44)

--- contracts/AccessTokenConsumer.sol
+++ contracts/AccessTokenConsumer.sol
@@ -3,14 +3,30 @@ pragma solidity >=0.8.13;
 
 import { IAccessTokenVerifier, AccessToken, FunctionCall } from "./interfaces/IAccessTokenVerifier.sol";
 
+/**
+ * @title AccessTokenConsumer
+ * @dev The AccessTokenConsumer contract is inherited by contracts that need to gate functions
+ * using Ethereum Access Tokens (EIP-7272).
+ * It provides the `requiresAuth` modifier which reverts unless a valid EAT is passed along with the function call.
+ * This contract stores and points to an AccessTokenVerifier contract which is called to verify
+ * an EAT after reconstructing it from a function call.
+ * Finally, it keeps track of consumed EATs to prevent re-use.
+ */
 contract AccessTokenConsumer {
+    // Address of the AccessTokenVerifier currently used by this contract
     IAccessTokenVerifier public verifier;
+    // Stores whether an EAT has already been used
     mapping(bytes32 => bool) private _accessTokenUsed;
 
     constructor(address accessTokenVerifier) {
         verifier = IAccessTokenVerifier(accessTokenVerifier);
     }
 
+    /**
+     * @dev Adds a layer of access control to a function
+     * by requiring a valid EAT to be provided.
+     * If valid, it marks the EAT as consumed.
+     */
     modifier requiresAuth(
         uint8 v,
         bytes32 r,
@@ -30,6 +46,10 @@ contract AccessTokenConsumer {
         verifier = IAccessTokenVerifier(newVerifier);
     }
 
+    /**
+     * @dev Verifies an EAT by calling the AccessTokenVerifier.
+     * Reverts if the EAT has already been used.
+     */
     function verify(uint8 v, bytes32 r, bytes32 s, uint256 expiry) internal view returns (bool) {
         // AU -> Already Used
         require(!_isAccessTokenUsed(v, r, s, expiry), "AccessToken: AU");
@@ -38,6 +58,10 @@ contract AccessTokenConsumer {
         return verifier.verify(token, v, r, s);
     }
 
+    /**
+     * @dev Reconstructs an EAT based on `expiry` and current function call
+     * to populate function signature, target contract address, caller and function parameters.
+     */
     function constructToken(uint256 expiry) internal view returns (AccessToken memory token) {
         FunctionCall memory functionCall;
         functionCall.functionSignature = msg.sig;
@@ -49,9 +73,11 @@ contract AccessTokenConsumer {
         token.expiry = expiry;
     }
 
-    // Takes calldata and extracts non-signature, non-expiry function inputs as a byte array
-    // Removes all references to the proof object except any offsets related to
-    // other inputs that are pushed by the proof
+    /**
+     * @dev Takes calldata and extracts non-signature, non-expiry function inputs as a byte array
+     * Removes all references to the proof object except any offsets related to
+     *  other inputs that are pushed by the proof
+     */
     function extractInputs() public pure returns (bytes memory inputs) {
         // solhint-disable-next-line no-inline-assembly
         assembly {
@@ -82,11 +108,17 @@ contract AccessTokenConsumer {
         }
     }
 
+    /**
+     * @dev Returns whether an EAT has already been used.
+     */
     function _isAccessTokenUsed(uint8 v, bytes32 r, bytes32 s, uint256 expiry) internal view returns (bool) {
         bytes32 accessTokenHash = keccak256(abi.encodePacked(v, r, s, expiry));
         return _accessTokenUsed[accessTokenHash];
     }
 
+    /**
+     * @dev Marks an EAT as consumed/used.
+     */
     function _consumeAccessToken(uint8 v, bytes32 r, bytes32 s, uint256 expiry) private {
         bytes32 accessTokenHash = keccak256(abi.encodePacked(v, r, s, expiry));
 
--- contracts/upgradeable/AccessTokenConsumerUpgradeable.sol
+++ contracts/upgradeable/AccessTokenConsumerUpgradeable.sol
@@ -5,11 +5,20 @@ import { Initializable } from "@openzeppelin/contracts-upgradeable/proxy/utils/I
 import { IAccessTokenVerifier, AccessToken, FunctionCall } from "../interfaces/IAccessTokenVerifier.sol";
 
 /**
- * @dev Implementation of `AccessTokenConsumer,` for consuming Ethereum Access Tokens (EIP-7272),
+ * @title AccessTokenConsumerUpgradeable
+ * @dev Implementation of `AccessTokenConsumer`, for consuming Ethereum Access Tokens (EIP-7272),
  * compatible with OpenZeppelin UUPS upgradeable contracts.
+ * @dev The AccessTokenConsumer contract is inherited by contracts that need to gate functions
+ * using Ethereum Access Tokens (EIP-7272).
+ * It provides the `requiresAuth` modifier which reverts unless a valid EAT is passed along with the function call.
+ * This contract stores and points to an AccessTokenVerifier contract which is called to verify
+ * an EAT after reconstructing it from a function call.
+ * Finally, it keeps track of consumed EATs to prevent re-use.
  */
 contract AccessTokenConsumerUpgradeable is Initializable {
+    // Address of the AccessTokenVerifier currently used by this contract
     IAccessTokenVerifier public verifier;
+    // Stores whether an EAT has already been used
     mapping(bytes32 => bool) private _accessTokenUsed;
 
     /**
@@ -22,6 +31,11 @@ contract AccessTokenConsumerUpgradeable is Initializable {
         }
     }
 
+    /**
+     * @dev Adds a layer of access control to a function
+     * by requiring a valid EAT to be provided.
+     * If valid, it marks the EAT as consumed.
+     */
     modifier requiresAuth(
         uint8 v,
         bytes32 r,
@@ -41,6 +55,10 @@ contract AccessTokenConsumerUpgradeable is Initializable {
         verifier = IAccessTokenVerifier(newVerifier);
     }
 
+    /**
+     * @dev Verifies an EAT by calling the AccessTokenVerifier.
+     * Reverts if the EAT has already been used.
+     */
     function verify(uint8 v, bytes32 r, bytes32 s, uint256 expiry) internal view returns (bool) {
         // AU -> Already Used
         require(!_isAccessTokenUsed(v, r, s, expiry), "AccessToken: AU");
@@ -49,6 +67,10 @@ contract AccessTokenConsumerUpgradeable is Initializable {
         return verifier.verify(token, v, r, s);
     }
 
+    /**
+     * @dev Reconstructs an EAT based on `expiry` and current function call
+     * to populate function signature, target contract address, caller and function parameters.
+     */
     function constructToken(uint256 expiry) internal view returns (AccessToken memory token) {
         FunctionCall memory functionCall;
         functionCall.functionSignature = msg.sig;
@@ -60,9 +82,11 @@ contract AccessTokenConsumerUpgradeable is Initializable {
         token.expiry = expiry;
     }
 
-    // Takes calldata and extracts non-signature, non-expiry function inputs as a byte array
-    // Removes all references to the proof object except any offsets related to
-    // other inputs that are pushed by the proof
+    /**
+     * @dev Takes calldata and extracts non-signature, non-expiry function inputs as a byte array
+     * Removes all references to the proof object except any offsets related to
+     *  other inputs that are pushed by the proof
+     */
     function extractInputs() public pure returns (bytes memory inputs) {
         // solhint-disable-next-line no-inline-assembly
         assembly {
@@ -85,11 +109,17 @@ contract AccessTokenConsumerUpgradeable is Initializable {
         }
     }
 
+    /**
+     * @dev Returns whether an EAT has already been used.
+     */
     function _isAccessTokenUsed(uint8 v, bytes32 r, bytes32 s, uint256 expiry) internal view returns (bool) {
         bytes32 accessTokenHash = keccak256(abi.encodePacked(v, r, s, expiry));
         return _accessTokenUsed[accessTokenHash];
     }
 
+    /**
+     * @dev Marks an EAT as consumed/used.
+     */
     function _consumeAccessToken(uint8 v, bytes32 r, bytes32 s, uint256 expiry) private {
         bytes32 accessTokenHash = keccak256(abi.encodePacked(v, r, s, expiry));
 
